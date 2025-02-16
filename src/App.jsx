2563757f0216b90f8fc791df6717e3c697c73f33
Kennedy-123 <okolochidera223@gmail.com>
2025-02-16 13:34:46 +0100

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import checkLoginStatus from "./utils/check-login";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Welcome from "./pages/Welcome";
+import FallBackLoader from "./components/FallBackLoader";
 
 const Login = lazy(() => import("./pages/Login"));
 const Signup = lazy(() => import("./pages/Signup"));
 const TrackedProduct = lazy(() => import("./pages/TrackedProduct"));
-const Home = lazy(() => import("./pages/Home"));
 const PricingPage = lazy(() => import("./pages/pricing"));
-const Welcome = lazy(() => import("./pages/Welcome"));
 const PaymentSuccess = lazy(() => import("./pages/PaymentSuccess"));
-const Navbar = lazy(() => import("./components/Navbar"));
-const Footer = lazy(() => import("./components/Footer"));
 
 const loginStatus = await checkLoginStatus();
 
 function App() {
   return (
     <Router>
-      <Suspense>
-        {loginStatus && <Navbar />}
+      {loginStatus && <Navbar />}
+      <Suspense fallback={<FallBackLoader />}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Welcome" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/TrackedProduct" element={<TrackedProduct />} />
           <Route path="/pricing" element={<PricingPage />} />
-          <Route path="/Welcome" element={<Welcome />} />
           <Route path="/payment-success" element={<PaymentSuccess />} />
         </Routes>
         <Footer />
---
+++ src/components/FallBackLoader.jsx
@@ -0,0 +1,9 @@
+const FallBackLoader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-4 border-orange-500 border-t-transparent"></div>
+    </div>
+  );
+};
+
+export default FallBackLoader;
